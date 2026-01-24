eb143114094b53334baf085eec5cdff63b68e630
iago-silves <iago.rodrig.silvestre@gmail.com>
2026-01-24 10:59:43 -0300

    add exercicio

---
+++ terceiro-mundo/exer72.py
@@ -0,0 +1,30 @@
+numeros = (
+    "zero",
+    "um",
+    "dois",
+    "três",
+    "quatro",
+    "cinco",
+    "seis",
+    "sete",
+    "oito",
+    "nove",
+    "dez",
+    "onze",
+    "doze",
+    "treze",
+    "quatorze",
+    "quinze",
+    "dezesseis",
+    "dezessete",
+    "dezoito",
+    "dezenove",
+    "vinte",
+)
+
+
+numero = int(input("Digite um número de 0 a 20: "))
+
+resultado = numeros[numero - 1]
+
+print(resultado)
